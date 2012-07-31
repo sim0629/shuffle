815b4a4f07e272f6cc0ab7ce356d67dcf618c5ff
shonenA <hitel00000@hotmail.com>
2012-07-31 18:42:03 +0900

    swfplayer 제대로 로딩 안되는 문제 수정

--- application/views/shuffle/playlist_swf.php
+++ application/views/shuffle/playlist_swf.php
@@ -1,15 +1,15 @@
 <div id="mp3_div"><a href="http://www.macromedia.com/go/getflashplayer">Get the Flash Player</a> to see this player.</div>
-<script type="text/javascript" src="<?php echo $swfobject ?>"></script>
+<script type="text/javascript" src="<?php echo base_url($swf_object) ?>"></script>
 <script type="text/javascript">
-    var so = new SWFObject("<?php echo $swf_mp3 ?>","mediaplayer","<?php echo $mp3_width ?>","<?php echo $mp3_height ?>","7");
+    var so = new SWFObject("<?php echo base_url($swf_mp3) ?>","mediaplayer","<?php echo $mp3_width ?>","<?php echo $mp3_height ?>","7");
     so.addVariable("repeat","always");
     so.addVariable("width","<?php echo $mp3_width ?>");
     so.addVariable("height","<?php echo $mp3_height ?>");
     so.addVariable("displayheight","0");
-    so.addVariable("file","<?php echo $loadfile ?>?" + (new Date().getTime()));
+    so.addVariable("file","<?php echo base_url($loadfile) ?>?" + (new Date().getTime()));
     so.addVariable("autostart","true");
     so.addVariable("shuffle","<?php echo $shuf ?>");
-    so.addVariable("skin","simple.swf");
+    so.addVariable("skin","<?php echo base_url('simple.swf') ?>");
     so.addVariable("playlist","bottom");
     so.addVariable("playlistsize","380");
     so.addVariable("lightcolor","cc0022");
